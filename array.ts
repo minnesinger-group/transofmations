0f9552b70e163b17878c4aba014aedb655393aec
andrewkuryan <Andrew.Kuryan@tut.by>
2023-08-30 14:54:56 +0300

    Init module and add uint8ArrayToBase64 function

---
+++ array.ts
@@ -0,0 +1,7 @@
+export function uint8ArrayToBase64(bytes: Uint8Array) {
+  let binary = '';
+  for (let i = 0; i < bytes.byteLength; i++) {
+    binary += String.fromCharCode(bytes[i]);
+  }
+  return window.btoa(binary);
+}
